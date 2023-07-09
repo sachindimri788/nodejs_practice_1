757b76c6dfda3ff18ec8b693390e359d7c387093
sachindimri788 <sachindimri788@gmail.com>
2023-07-09 15:54:11 +0530

    3.routerandfilter

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ const express=require('express');
 const app=express();
 const bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded());
+const adminRoutes=require('./routes/admin')
+const shopRouter=require('./routes/shop')
 
-app.use('/add_product',(req,res,next)=>{
-  res.send('<form action="/product" method="POST"> NAME: <input type="text" name="title"> <br> Size: <input type="text" name="size"> <br> <input type="submit"> </form>')
-  next();
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRouter);
+
+app.use('/',(req,res,next)=>{
+   res.status(404).send('<h1>Page not found</h1>')
 })
-
-app.use('/product',(req,res,next)=>{
-  console.log(req.body)
-  res.redirect('/add_product');
-})
-
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
