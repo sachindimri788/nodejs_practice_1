d1a05dad67243444934812fc903bea2a9697d0f0
sachindimri788 <sachindimri788@gmail.com>
2023-07-11 23:10:17 +0530

    5.html,css,nodejs,views

---
+++ routes/admin.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+router.get('/contactus',(req,res,next)=>{
+  res.sendFile(path.join(rootDir,'views','contactus.html'));
+})
+
+router.post('/contactus',(req,res,next)=>{
+  res.sendFile(path.join(rootDir,'views','success.html'));
+})
+
+module.exports = router;
